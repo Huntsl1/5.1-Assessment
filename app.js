cf6c6241f2c38122810966c85c6c4cfd51207683
Huntsl1 <hunt.stephen.l@gmail.com>
2017-06-19 16:49:06 -0400

    second and final commit, fixed errors

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
+var loremIpsum = require('lorem-ipsum')
 
 const app = express();
 
-var loremIpsum = require('lorem-ipsum')
 var output = loremIpsum({
   count: 3,
   units: 'paragraphs',
+  sentenceLowerBound: 8,
+  sentenceUpperBound: 10,
+  paragraphLowerBound: 10,
+  paragraphUpperBound: 15,
+  format: 'html',
 });
 
 //app.use('/lorem',express.static("public"));
 
 app.get('/lorem', function(req, res){
   res.send(output);
-  console.log(loremIpsum);
+  //console.log(loremIpsum);
 
 });
 
